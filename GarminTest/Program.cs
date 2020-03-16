871cb3d7b82145b8cc6de5299c27481faa9d160c
Muhammad Miftah <mamift@gmail.com>
2020-03-16 10:14:22 +1000

    Add project files.

---
+++ GarminTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Garmin.TrainingCenterDatabase.V2;
+
+namespace GarminTest
+{
+    public class Program
+    {
+        private const string NS_GPX_TrainingCenterDatabase_v2 =
+            "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2";
+
+        static void Main()
+        {
+            var fileName = @"trainingCenterDbExample1.xml";
+
+            Console.WriteLine("Loading example training center DB...");
+            var tcdb = TrainingCenterDatabase.Load(fileName);
+
+            Debug.Assert(tcdb.Activities.Activity.Any());
+            Console.WriteLine($"{tcdb.Untyped}");
+        }
+    }
+}
